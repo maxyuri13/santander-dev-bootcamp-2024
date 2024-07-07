2728c3f3108d3c5d15bb3c81d8d852a7b9649311
Max Sousa <max.yuri.13@hotmail.com>
2024-07-07 14:03:57 -0300

    Add classes and getter/setter

--- src/main/java/me/dio/domain/Application.java
+++ src/main/java/me/dio/domain/Application.java
@@ -1,4 +1,4 @@
-package me.dio.santander_dev_bootcamp_2024;
+package me.dio.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/me/dio/domain/SantanderDevBootcamp2024ApplicationTests.java
+++ src/test/java/me/dio/domain/SantanderDevBootcamp2024ApplicationTests.java
@@ -1,4 +1,4 @@
-package me.dio.santander_dev_bootcamp_2024;
+package me.dio.domain;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
